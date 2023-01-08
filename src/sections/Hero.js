ec99d7f8e82dc343302e6f6ef4b1cd7a6f3d3b43
John Canero <johncanero.official@gmail.com>
2023-01-08 18:38:11 +0800

    Change the Body Background Color and Add Font Style: Roboto

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="text-center">
+    <div className="mt-6 text-center font-Roboto">
       <h1 className="text-3xl font-bold text-blue-500 ">Weather App React</h1>
       <p>country, cities, state</p>
     </div>
