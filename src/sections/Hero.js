0d32d9e4f64f69bf596c0fa4ed83352eda1383fd
John Canero <johncanero.official@gmail.com>
2023-01-08 17:18:07 +0800

    first commit

---
+++ src/sections/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-blue-500 ">Weather App React</h1>
+      <p>country, cities, state</p>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+  
+      }
+
+    },
+    fontFamily: {
+      // Poppins: ['Poppins', 'sans-serif'],
+      // Montserrat: ['Montserrat', 'sans-serif'],
+    },
+ 
+  },
+  plugins: [],
+}
