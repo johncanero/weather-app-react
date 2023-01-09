993459c5e59a339863e9c13f10a9adc5edad1d83
John Canero <johncanero.official@gmail.com>
2023-01-09 21:18:34 +0800

    lmplement loadOptions Variable and GeoDB Cities

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a76aa5d2abmshd13ac37184dcf79p13c51cjsn189dac2acc55',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
--- src/components/search.js
+++ src/components/search.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+
+  // API
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions,
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
   };
+
   return (
     <div>
-      <div className="container mx-auto mt-2">
+      <div className="container max-w-6xl mx-auto mt-2">
         <AsyncPaginate
           placeholder="Search for city"
           debounceTimeout={600}
           value={search}
           onChange={handleOnChange}
+          loadOptions={loadOptions}
         />
       </div>
     </div>
