3dde55be75419d50673e72700db2ddcba6a21450
John Canero <johncanero.official@gmail.com>
2023-01-10 10:38:57 +0800

    Add Details and Complete Static Information of Weather

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -3,6 +3,7 @@ import "./current-weather.css";
 const CurrentWeather = () => {
   return (
     <div className="weather">
+    {/* Top */}
       <div className="top">
         {/* City and Weather */}
         <div>
@@ -13,17 +14,34 @@ const CurrentWeather = () => {
         <img alt="weather" className="weather-icon" src="/icons/01d.png" />
       </div>
 
+      {/* Bottom */}
       <div className="bottom">
         {/* Temperature */}
         <p className="temperature">18°C</p>
         <div className="details">
-            <div className="parameter-row">
-                <span className="parameter-label">Details</span>
-            </div>
-            <div className="parameter-row">
-                <span className="parameter-label">Feels like</span>
-                <span className="parameter-value">22°C</span>
-            </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          {/* Feels like */}
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          {/* Wind */}
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          {/* Humidity */}
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15 %</span>
+          </div>
+          {/* Pressure */}
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+          </div>
         </div>
       </div>
     </div>
