04e4672bfbea7e343bf29754bcabab92bf35f809
John Canero <johncanero.official@gmail.com>
2023-01-10 10:24:27 +0800

    Add Temperature and Details

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -12,6 +12,20 @@ const CurrentWeather = () => {
         {/* Icon */}
         <img alt="weather" className="weather-icon" src="/icons/01d.png" />
       </div>
+
+      <div className="bottom">
+        {/* Temperature */}
+        <p className="temperature">18°C</p>
+        <div className="details">
+            <div className="parameter-row">
+                <span className="parameter-label">Details</span>
+            </div>
+            <div className="parameter-row">
+                <span className="parameter-label">Feels like</span>
+                <span className="parameter-value">22°C</span>
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
