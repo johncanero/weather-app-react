7d45fa224cf320a1b0d5a8e41d0f43e1b8c80e83
John Canero <johncanero.official@gmail.com>
2023-01-11 15:42:23 +0800

    Put API Keys to Environment Variable

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'a76aa5d2abmshd13ac37184dcf79p13c51cjsn189dac2acc55',
+		'X-RapidAPI-Key': process.env.REACT_APP_GEO_API_KEY,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 // API URL and KEY: OpenWeather Mao
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "771c320d0040bbe2fa426aeb2e968fda"; // enter your key from openweather API
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY; // enter your key from openweather API
